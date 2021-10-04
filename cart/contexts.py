8f03e7a4bdc24517a1521ddcf53b4ddc8a74862c
Puksrevolution <puksbusinessworld@gmail.com>
2021-10-04 22:03:27 +0000

    Dev: Added cart context and delivery logic

---
+++ cart/contexts.py
@@ -0,0 +1,67 @@
+"""
+Code adapted from the Code Institute Boutique Ado mini project
+Context processor to make the cart_contents returned dictionary
+available to all templates across the entire application
+"""
+
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.discount_price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.discount_price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+
+        # Check if the standard delivery is more than the minimum delivery cost
+        if ((total / Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)) <
+                Decimal(settings.MINIMUM_DELIVERY_COST) and total > 0):
+            delivery = Decimal(settings.MINIMUM_DELIVERY_COST)
+        else:
+            delivery = total / Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
+    }
+
+    return context
