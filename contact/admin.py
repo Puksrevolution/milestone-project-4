0705dc2f046ea54c406d2da6085f5bcd22ef037e
Puksrevolution <puksbusinessworld@gmail.com>
2021-11-07 15:29:55 +0000

    Dev: Added admin function

--- contact/admin.py
+++ contact/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Contact
 
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    model = Contact
+    readonly_fields = (
+        'query_title',
+        'query_text',
+        'query_email',
+        'date_posted',
+        'query_from',
+        )
+
+
+admin.site.register(Contact, ContactAdmin)
